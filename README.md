acf55321187944d688c060fe9460e8694f2624d5
Raneen-z <raneen.alzafarani@tuwaiq.edu.sa>
2021-05-25 20:20:05 +0300

    add files

---
+++ README.md
@@ -0,0 +1,37 @@
+# Intro to React
+
+- What and why ReactJS 
+- What is React components  
+- Create class components
+- Pass data to other components (props)
+
+
+To create new react app:
+<br>
+`cd React-Session`
+<br>
+`npx create-react-app react-intro`
+<br>
+`code .`
+<br>
+`cd react-intro`
+<br>
+`npm start`
+<br>
+
+to use this react app:
+<br>
+`git clone https://github.com/Raneen-z/React-Session.git`
+<br>
+`cd React-Session`
+<br>
+`code .`
+<br>
+`cd react-intro`
+<br>
+`npm install` 
+<br>
+`cd react-intro`
+<br>
+`npm start`
+<br>
---
+++ react-intro/src/App.js
@@ -0,0 +1,55 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+// function App() {
+//   return (
+//     <div>
+     
+//       <h2> قائمة الكتب </h2>
+
+//       <img src={img1}/>
+//       <h4>خواطر السنين</h4>
+//       <h5>الكاتب: محمد مكية</h5>
+//       <hr/>
+
+//       <img src={img2}/>
+//       <h4>العصفورية</h4>
+//       <h5>الكاتب: غازي القصيبي</h5>
+//       <hr/>
+
+//       <img src={img3}/>
+//       <h4>ورددت الجبال الصدى</h4>
+//       <h5>الكاتب: خالد حسيني</h5>
+//       <hr/>
+                
+//       <div class="footer">
+//         <h3>@2021 | Tuwaiq Academy</h3>
+//       </div>
+    
+      
+//     </div>
+//   );
+// }
+
+export default App;
